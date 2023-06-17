e608f9d85d6e944520e37cb40feb626e64f8d810
ashmarin_yuriy <ashmarin.yuriy@gmail.com>
2023-06-17 16:12:30 +0300

    feat: create new code

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.Write("Введи трехзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine("Вторая цифра введенного числа -> "+stringNumber[1]);
