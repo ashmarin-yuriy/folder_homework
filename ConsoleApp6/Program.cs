b58559ada6e16d80780d2069ceafb0d51dd675b2
ashmarin_yuriy <ashmarin.yuriy@gmail.com>
2023-06-17 16:19:38 +0300

    feat: create new code

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. */
+
+Console.Write("Введите число: ");
+
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine("Третья цифра " + anyNumberText[2]);
+}
+else
+{
+  Console.WriteLine(" Третьей цифры нет");
+}
