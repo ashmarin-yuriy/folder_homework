7e5ef9f01f841f89e2ac1f68adda87d98a2d34ae
ashmarin_yuriy <ashmarin.yuriy@gmail.com>
2023-06-17 16:39:03 +0300

    feat: create new code

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿/* Напишите программу,
+которая принимает на вход цифру,
+обозначающую день недели, и проверяет,
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber)
+{
+  if (dayNumber == 6 || dayNumber == 7)
+  {
+  Console.WriteLine("(Этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7)
+  {
+    Console.WriteLine("В неделе семь дней");
+  }
+  else Console.WriteLine("(Этот день не выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+
