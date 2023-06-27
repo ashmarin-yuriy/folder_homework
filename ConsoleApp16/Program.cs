1630290018b220da078a2d7567e715b44446c9f2
ashmarin_yuriy <ashmarin.yuriy@gmail.com>
2023-06-27 20:40:31 +0300

    feat:create new code

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным 
+и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76 */
+
+double[] array = new double[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(new Random().NextDouble() * new Random().Next(0, 10), 2);
+    Console.WriteLine(array[i] + "  ");
+}
+
+double min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array [i] < min)
+    {
+        min = array [i];
+    }
+}
+
+Console.WriteLine($"Минимальное число: {min}");
